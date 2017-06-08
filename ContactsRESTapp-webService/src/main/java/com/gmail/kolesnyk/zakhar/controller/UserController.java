cb9b40223d91164f3cd2f89ab278c7b896175ded
ZakharKolesnyk <zakhar.kolesnyk@gmail.com>
2017-06-08 11:31:58 +0300

    replace Gson methods in Controller to methods "jsonp-jaxrs"  from glassfish lib (JSR 353)

--- ContactsRESTapp-webService/src/main/java/com/gmail/kolesnyk/zakhar/controller/UserController.java
+++ ContactsRESTapp-webService/src/main/java/com/gmail/kolesnyk/zakhar/controller/UserController.java
@@ -2,7 +2,6 @@ package com.gmail.kolesnyk.zakhar.controller;
 
 import com.gmail.kolesnyk.zakhar.service.userService.UserService;
 import com.gmail.kolesnyk.zakhar.user.User;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,27 +16,27 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json")
-    public String createUser(@RequestBody User user) throws IOException {
-        return new Gson().toJson(userService.create(user));
+    public User createUser(@RequestBody User user) throws IOException {
+        return userService.create(user);
     }
 
     @ResponseBody
     @RequestMapping(value = "/id/{id}", method = RequestMethod.GET, produces = "application/json")
-    public String getUserById(@PathVariable Integer id) throws IOException {
-        return new Gson().toJson(userService.getById(id));
+    public User getUserById(@PathVariable Integer id) throws IOException {
+        return userService.getById(id);
     }
 
     @ResponseBody
     @RequestMapping(value = "/name/{name}", method = RequestMethod.GET, produces = "application/json")
-    public String getUserByName(@PathVariable String name) throws IOException {
-        return new Gson().toJson(userService.getByName(name));
+    public User getUserByName(@PathVariable String name) throws IOException {
+        return userService.getByName(name);
     }
 
     @ResponseBody
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
-    public String updateUser(@PathVariable Integer id, @RequestBody User user) throws IOException {
+    public User updateUser(@PathVariable Integer id, @RequestBody User user) throws IOException {
         user.setId(id);
-        return new Gson().toJson(userService.update(user));
+        return userService.update(user);
     }
 
     @ResponseBody
